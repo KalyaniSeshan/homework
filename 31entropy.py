68cba345b4fc3cbdd2b59f2b9a7e7ce50a88d811
Kalyani Seshan <122393598+KalyaniSeshan@users.noreply.github.com>
2023-02-20 23:06:39 -0800

    Update 31entropy.py

--- 31entropy.py
+++ 31entropy.py
@@ -10,6 +10,16 @@
 
 # Hint: try breaking your program with erroneous input
 
+import math
+
+vals = [0.1, 0.2, 0.3, 0.4]
+
+assert(math.isclose(sum(vals), 1.0))
+
+H = 0
+for val in vals:
+	H -= val * math.log2(val)
+print(f'{H:.3f}')
 
 """
 python3 31entropy.py 0.1 0.2 0.3 0.4
