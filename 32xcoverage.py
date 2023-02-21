2617d5e6856aa3d3e66ad237b2f64d8265366cd0
KalyaniSeshan <kaly.sesh20@gmail.com>
2023-02-20 17:08:53 -0800

    updated

--- 32xcoverage.py
+++ 32xcoverage.py
@@ -14,8 +14,36 @@
 
 # Note: you will not get exactly the same results as the command line below
 
+import sys
+import random
+
+genome = []
+genome_size = int(sys.argv[1])
+read_num = int(sys.argv[2])
+read_len = int(sys.argv[3])
+
+for i in range(genome_size):
+	genome.append(0)
+
+end = genome_size - read_len
+for start in range(read_len):
+	start = random.randint(0, end)
+	for i in range(start, start + read_len):
+		genome[i] += 1
+		
+minimum = genome[read_len: - read_len]
+maximum = genome[read_len: - read_len]
+avg_coverage = sum(genome[read_len: - read_len]) / len(genome)
+print(min(minimum), max(maximum), f'{avg_coverage:.5f}')
 
 """
+end = genome_size - read_len
+for read in range(read_num):
+	start = random.randint(0, end)
+	for n in range(start, start + read_len):
+		genome[i] += 1
+
 python3 32xcoverage.py 1000 100 100
 5 20 10.82375
 """
+
