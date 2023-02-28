e2e844a7617916f6a6d8bbfc08466576e06d88db
KalyaniSeshan <kaly.sesh20@gmail.com>
2023-02-27 22:16:00 -0800

    updated

--- 41transmembrane.py
+++ 41transmembrane.py
@@ -21,6 +21,51 @@
 # Hint: create a function for hydrophobic alpha-helix
 # Hint: use the same function for both signal peptide and transmembrane
 
+import mcb185
+import sys
+
+def kd_hydro(aa):
+	if aa == 'I': return 4.5
+	if aa == 'V': return 4.2
+	if aa == 'L': return 3.8
+	if aa == 'F': return 2.8
+	if aa == 'C': return 2.5
+	if aa == 'M': return 1.9
+	if aa == 'A': return 1.8
+	if aa == 'G': return -0.4
+	if aa == 'T': return -0.7
+	if aa == 'S': return -0.8
+	if aa == 'W': return -0.9
+	if aa == 'Y': return -1.3
+	if aa == 'P': return -1.6
+	if aa == 'H': return -3.2
+	if aa == 'E': return -3.5
+	if aa == 'Q': return -3.5
+	if aa == 'D': return -3.5
+	if aa == 'N': return -3.5
+	if aa == 'K': return -3.9
+	if aa == 'R': return -4.5
+	return 0
+		
+def a_helix(seq, w, t):
+	hydro = 0
+	for aa in seq:
+		hydro += kd_hydro(aa)
+	for i in range(len(seq) - w + 1):
+		window = seq[i : i + w]
+		if 'P' in window: 
+			continue
+		elif hydro / w < t: 
+			continue
+		else: 
+			return True
+	return False
+		
+for defline, seq in mcb185.read_fasta(sys.argv[1]):
+	if a_helix(seq[0:30], 8, 2.5) and a_helix(seq[30:], 11, 2.0):
+		print(defline)
+
+
 
 """
 python3 41transmembrane.py ~/DATA/E.coli/GCF_000005845.2_ASM584v2_protein.faa.gz
--- 42dust.py
+++ 42dust.py
@@ -12,8 +12,45 @@
 
 # Note: don't worry about "centering" the entropy on the window (yet)
 
+import mcb185
+import sys
+import math
+
+def ent(vals):
+	assert(math.isclose(sum(vals), 1.0))
+	H = 0
+	for val in vals:
+		if val != 0: H -= val * math.log2(val)
+	return H
+	
+def seqent(seq):
+	A = seq.count('A') / len(seq)
+	C = seq.count('C') / len(seq)
+	T = seq.count('T') / len(seq)
+	G = seq.count('G') / len(seq)
+	H = ent([A, C, T, G])
+	return H
+
+w = int(sys.argv[2])
+entthresh = float(sys.argv[3])
+
+# print(w, entthresh) to test
+
+for defline, seq in mcb185.read_fasta(sys.argv[1]):
+	seq1 = list(seq)
+	for i in range(len(seq)):
+		if seqent(seq[i:i+w]) < entthresh:
+			seq1[i] = 'N'
+	seq = ''.join(seq1)
+print(seq)
 
 """
+		else:
+			seq1 += seq[i]
+	for i in range(0
+	seq1 = 'N' + seq[i + 1 : i + w]
+		
+
 python3 42dust.py ~/DATA/E.coli/GCF_000005845.2_ASM584v2_genomic.fna.gz 11 1.4
 >NC_000913.3 Escherichia coli str. K-12 substr. MG1655, complete genome
 AGNTTTTCATTCTGACTGCAACGGGCAATATGTCTCTGTGTNNNNNNNAAAAAGAGTGTC
